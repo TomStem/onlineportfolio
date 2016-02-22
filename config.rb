7d43fc5355c5337233b0cf0c98406ece6c52ae52
Tom Stemerding <tom@stemerdingmail.nl>
2016-02-22 16:26:53 +0100

    changes changes changes

--- config.rb
+++ config.rb
@@ -90,6 +90,6 @@ activate :deploy do |deploy|
   # deploy.commit_message = 'custom-message' # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
 end
 
-["kraamzorgdezuster", "kusted", "zango", "schoolproject"].each do |project_name|
+data.projects.each do |project_name, info|
   proxy "#{project_name}.html", "show.html", :locals => { :owner => project_name }, :ignore => true
 end
