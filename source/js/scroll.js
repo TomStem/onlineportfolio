b00e85c96b6bd6ba7e2dfa7c0cca81f15704c928
Tom Stemerding <tom@stemerdingmail.nl>
2016-02-25 16:45:28 +0100

    d

--- source/js/scroll.js
+++ source/js/scroll.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   
-  //Check to see if the window is top if not then display button
+//   //Check to see if the window is top if not then display button
   $(window).scroll(function(){
     if ($(this).scrollTop() > 100) {
       $('.scrollToTop').fadeIn();
@@ -8,11 +8,21 @@ $(document).ready(function(){
       $('.scrollToTop').fadeOut();
     }
   });
-  
-  //Click event to scroll to top
-  $('.scrollToTop').click(function(){
-    $('html, body').animate({scrollTop : 0},800);
-    return false;
+
+});
+
+
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
   });
-  
 });
